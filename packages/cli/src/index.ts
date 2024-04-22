6119171c1b6b5ac6a63bb95e99e764965fbaafcc
Goblin Oats <hello@tonk.gg>
2024-04-22 18:55:12 +0100

    refactor: stub out potential cli command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,25 +4,21 @@ const program: Command = new Command();
 
 program
   .name("gribi-cli")
-  .description("Utility for working with gribi projects")
+  .description("Gribi command line tool helps you to work with Gribi in your projects.")
   .version("0.0.1");
 
 // Init command
-program
+const module = program
+  .command('module')
+  .description('Helpful actions when working with modules')
+
+module
   .command('init')
-  .description('Initialize a new project config file')
+  .argument('<template>', 'description or path to module template')
+  .description('Initialize a new module using the module template')
   .action(() => {
     console.log(`This isn't working yet`);
     // Add your init logic here
-  });
-
-// Generate command
-program
-  .command('generate')
-  .description('Generate codefiles from your gribi.config.ts')
-  .action(() => {
-    console.log('Generation process started...');
-    // Add your generate logic here
   });
 
 // Help command is built-in, but you can customize it
